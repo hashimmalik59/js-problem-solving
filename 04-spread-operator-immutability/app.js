10e492a39e2451083adcba2e60b8d17f9a16150d
hashimmalik59 <hm3630450@gmail.com>
2026-06-11 22:48:38 +0500

    04-spread-operator-immutability, problem solving and confidence

---
+++ 04-spread-operator-immutability/app.js
@@ -0,0 +1,26 @@
+// const user = { id: 1, name: "Ali", role: "guest" };
+// const newUser = { ...user, name: "Hashim" };
+
+// console.log("ORIGINAL", user);
+// console.log("NEW", newUser);
+
+// const user = { name: "Ali", age: 20 };
+// const jobDetails = { job: "Engineer", city: "Lahore" };
+
+// const userProfile = { ...user, ...jobDetails };
+
+// console.log("OLD", user);
+// console.log("OLD", jobDetails);
+// console.log("NEW", userProfile);
+
+// const product = {
+//   id: 101,
+//   name: "Laptop",
+//   price: 1000,
+//   inStock: true,
+// };
+
+// const newProduct = { ...product, price: 1500 };
+
+// console.log("ORIGINAL", product);
+// console.log("NEW", newProduct);
